f68ccb02fd41a29b8fb2e2705713acf9d786a2ad
ThunderGer <jaames11@hotmail.com>
2022-11-24 11:02:14 -0600

    Prueba 3 railway

--- helpers/model.py
+++ helpers/model.py
@@ -1,9 +1,9 @@
-from tensorflow_hub import hub
+import tensorflow_hub as hub
 import tensorflow_text as text
 from tensorflow import keras
 
 new_model = keras.models.load_model(
     ('Cod_Red.h5'),
-    custome_objects = {'KerasLayer': hub.KerasLayer}
+    custom_objects = {'KerasLayer': hub.KerasLayer}
 )
 
