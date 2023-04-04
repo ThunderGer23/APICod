e0f89ddcd687d4b1dd125df9fa37f9cdea4928f7
ThunderGer <Jaames11@hotmail.com>
2023-04-03 23:10:28 -0600

    ajustes para notigram

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from docs.docs import tags_metadata
 from os import environ as env
 from notigram import ping
 
-ping('daa39d53-6283-47a1-b945-b7ee6528dde0', 'Servidor de código corriendo')
+ping(env['TOKEN'], 'Servidor de código corriendo')
 app = FastAPI(
     title= "Someone title :v/ vrgs",
     description= "Someone description :v/ prrna",
